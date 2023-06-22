9d6896c48f2ac2d71bc3e6b0c505b9bc26f5d824
alekkireev <alekkireev@gmail.com>
2023-06-22 23:42:10 +0400

    Unit tests for DictionaryExtensions

---
+++ KeepCorrect.Common.Extensions.Tests/DictionaryExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace KeepCorrect.Common.Extensions.Tests;
+
+public class DictionaryExtensionsTests
+{
+    [TestCase(1, "first")]
+    [TestCase(4, default(string))]
+    public void GetValueOrDefault_IfKeyIsNotFound_ReturnsDefaultOfValue(int key, string expected)
+    {
+        //Arrange
+        var dict = new Dictionary<int, string>
+        {
+            { 1, "first" },
+            { 2, "second" },
+            { 3, "third" }
+        };
+        //Act
+        var result = dict.GetValueOrDefault(key);
+        //Assert
+        Assert.That(expected, Is.EqualTo(result));
+    }
+}
